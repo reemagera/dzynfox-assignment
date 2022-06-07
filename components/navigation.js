a90d9d0cff2e08e7d459cf199ecb6ad87b14390d
reemagera <reemasgera@gmail.com>
2022-06-07 23:34:12 +0530

    added navigation bar

---
+++ components/navigation.js
@@ -0,0 +1,19 @@
+import Link from "next/link"
+import styles from "../components/navigation.module.css"
+export default function NavBar(){
+    return(
+        <header className={styles.headerbox}>
+            <div className={styles.logo}>
+                <Link href="/"><a className={styles.logoa}>My Page</a></Link>
+            </div>
+            <nav>
+                <ul className={styles.ul}>
+                    <li className={styles.li}><Link href="/"><a className={styles.a}>Home</a></Link></li>
+                    <li className={styles.li}><Link href=""><a className={styles.a}>About</a></Link></li>
+                    <li className={styles.li}><Link href=""><a className={styles.a}>Projects</a></Link></li>
+                    <li className={styles.li}><Link href=""><a className={styles.a}>Contact Us</a></Link></li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,9 @@
+// import Link from "next/link"
+import NavBar from "../components/navigation"
 export default function Home(){
     return(
         <>
+            <NavBar/>
             <h1>Starting to build landing page</h1>
         </>
     )
