bedb9fdbc88e147730de732a4d60aee573728033
reemagera <reemasgera@gmail.com>
2022-06-08 00:25:04 +0530

    did some updates to nav and msg comps

--- components/welcome.js
+++ components/welcome.js
@@ -1,8 +1,15 @@
+import Link from "next/link"
+import styles from "./welcome.module.css"
 export default function WelcomeMsg(){
     return(
-        <div className="">
-            <h4>Welcome to My Page</h4>
-            <h1>I am Reema Gera. I am an Engineering Student from VNIT Nagpur.This is my portfolio.</h1>
+        <div className={styles.container}>
+            <h3 className={styles.h3}>Welcome to My Page</h3>
+            <h1 className={styles.h1}>Hi! Glad you're here.<br/>I am Reema Gera. <br/>I am an Engineering Student <br/>from VNIT Nagpur. <br/>This is my portfolio.
+            </h1>
+            <div>
+                <Link href="/"><a>Let's collab</a></Link>
+                <Link href="/"><a>More About Me</a></Link>
+            </div>
         </div>
     )
 }
